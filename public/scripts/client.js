1dd878baec05d875dd2b250c6a157b1574be77a5
faridAbinah <faridabinah@hotmail.com>
2021-10-09 15:38:11 -0400

    Added functinality to fetch data asynchronously using jquery and ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,89 +4,96 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function(tweetObject) {
+$().ready(() => {
 
-  const $tweet = $(`
-  <div id="tweet-box" class="tweet-container">
-   <article class="tweet">
-  
-   <header>
-          <div class="flex">
-          <img class="avatar" src="${tweetObject.user.avatars}">
-          <p>
-            ${tweetObject.user.name}
-          <p>
-        </div>
-        <div>
-          <p>${tweetObject.user.handle}<p>
-        </div>
-        </header>
-        ${tweetObject.content.text}
-        <hr>
-        
-        <footer>
-         ${timeago.format(tweetObject.created_at)}
-          <div id="icons">
-          <a href=""><i class="fas fa-flag icons"></i></a>
-          <a href=""><i class="fas fa-retweet icons"></i></a>
-          <a href=""><i class="fas fa-heart icons"></i></a>
+  const createTweetElement = function(tweetObject) {
+
+    const escape = function (str) {
+
+      let div = document.createElement("div");
+      div.appendChild(document.createTextNode(str));
+      return div.innerHTML; 
+    }
+
+    const $tweet = $(`
+    <div id="tweet-box" class="tweet-container">
+     <article class="tweet">
+    
+     <header>
+            <div class="flex">
+            <img class="avatar" src="${tweetObject.user.avatars}">
+            <p>
+              ${tweetObject.user.name}
+            <p>
           </div>
-        </footer>
-   </article>
-   </div>
-  `
-  );
+          <div>
+            <p>${tweetObject.user.handle}<p>
+          </div>
+          </header>
+          ${escape(tweetObject.content.text)}
+          <hr>
+          
+          <footer>
+           ${timeago.format(tweetObject.created_at)}
+            <div id="icons">
+            <a href=""><i class="fas fa-flag icons"></i></a>
+            <a href=""><i class="fas fa-retweet icons"></i></a>
+            <a href=""><i class="fas fa-heart icons"></i></a>
+            </div>
+          </footer>
+     </article>
+     </div>
+    `
+    );
+    
   
+    return $tweet;
+  };
+  
+  const renderTweets = function(arrayOfTweets) {
+    // console.log(arrayOfTweets);
 
-  return $tweet;
-};
+    $('#tweet-section').empty();
 
-const renderTweets = function(arrayOfTweets) {
+    for(let userObject of arrayOfTweets) {
+      const tweet = createTweetElement(userObject);
+      $('#tweet-section').prepend(tweet);
+     
+    }
 
-  for(let userObject of arrayOfTweets) {
-    const tweet = createTweetElement(userObject);
-    $('#tweet-section').append(tweet);
+    
+  }
+
+  const loadTweets = function() {
+    // Get request using ajax and jquery
+    $.ajax({
+      url:"/tweets/",
+      methods:"GET",
+      dataType:"json",
+      success:(data) => {
+        // console.log("data: ",data);
+
+        renderTweets(data);
+      },
+      error: (err) => {
+        console.log(`error ${err}`);
+      }
+
+    })
+
+    // $.get("/tweets/",renderTweets);
+
    
   }
 
-}
-
-const tweetData = 
-  [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1633388593122
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1633474993122
-    }
-  ];
- 
-
-//const $tweet = createTweetElement(tweetData);
-
-
-$().ready(() => {
-
-  // $('#tweet-box')
-  // .after($tweet);
-  renderTweets(tweetData);
-
+  
+  
+  loadTweets();
+    //hide error messages 
+    $('#0-char-error').hide();
+    //hide error messages 
+    $('#max-char-error').hide();
+   
   $('#tweet-form').submit(function(event) {
    
     event.preventDefault();
@@ -94,13 +101,39 @@ $().ready(() => {
 
     console.log("form submitted", serializedData);
 
-    $.post("/tweets/",serializedData,function(sucess) {
+    console.log("text area value",$('#tweet-text').val().length);
 
-    console.log(serializedData,"Post request");
-    });
+    
+    //Form Validation
+    if($('#tweet-text').val().length === 0 ) {
+      $('#0-char-error').show();
+     
+    } else if($('#tweet-text').val().length > 140){
+
+      $('#max-char-error').show();
+
+    } else {
+
+       //hide error messages 
+    $('#0-char-error').hide();
+    //hide error messages 
+    $('#max-char-error').hide();
+
+      //Post request sending serialized data to the server
+      $.post("/tweets/",serializedData,function(success) {
+  
+      console.log(serializedData,"Post request",success);
+        
+      loadTweets();
+
+      });}
+
+    
   });
 
+  
 
+ 
 });
 
 
